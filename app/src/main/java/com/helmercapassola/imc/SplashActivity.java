d564cdad40b38014cb0e4ca8ffc779870afc1a99
helmer <helmer>
2019-12-21 11:20:33 +0100

    create activicy Splash

---
+++ app/src/main/java/com/helmercapassola/imc/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.helmercapassola.imc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final ProgressBar progressBar = findViewById(R.id.simpleProgressBar);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                progressBar.setVisibility(View.VISIBLE);
+                finish();
+            }
+        },3000);
+    }
+}
